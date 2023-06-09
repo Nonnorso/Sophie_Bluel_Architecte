f3eea0d9ea8b11a78ea0f06c3a19f0c75b9e8163
Nonnorso <r.philippot@live.fr>
2023-06-09 15:16:30 +0200

    reattribution de la classe css sur les boutons filtre

--- FrontEnd/assets/factory/createFilters.js
+++ FrontEnd/assets/factory/createFilters.js
@@ -2,6 +2,8 @@ function createFilters (categoryId, text) {
     let button = document.createElement('button')
     button.setAttribute('data-cat-id', categoryId)
     button.textContent = text
+    button.classList.add('btnFilter')
+    button.classList.add('btnFilter:hover')
 
     return button
 }
